3ec1cb9cf9a97f92464f9bc7a9f86e320178b614
Ponce <lmp@brreg.no>
2019-08-01 07:53:49 +0200

    Project Object

---
+++ src/main/java/lmp/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,13 @@
+package lmp.ppmtool.repository;
+
+
+import lmp.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
